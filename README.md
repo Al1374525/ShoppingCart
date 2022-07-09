85440a05495238b7f9c8a0033970798f86646d61
Al1374525 <albertayiteyadjin@gmail.com>
2022-07-09 00:17:09 -0400

    We are creating an remove and add itens

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ShoppingCart
+https://stackoverflow.com/questions/44104644/shopping-cart-function-in-python
--- src/main/shoppingCart.py
+++ src/main/shoppingCart.py
@@ -1,1 +1,23 @@
-def ShoppingCart():
+class ShoppingCart():
+
+    def __init__(self):
+        self.total = 0
+        self.cart = {}
+    #i want to be able to add items to the cart
+    def add_items(self, item_name, quantity, price):
+        self.cart[item_name] = quantity
+        self.total +=  price * quantity
+    
+    def remove_items(self, item_name, quantity, price):
+        if(quantity < self.cart[item_name] and quantity >= 0):  
+            self.cart[item_name] -= quantity
+        elif(quantity >= self.cart[item_name]):
+            del self.cart[item_name]
+    
+    def check_out(self, cash_paid):
+        if (cash_paid >= self.total):
+            return cash_paid - self.total
+
+    
+
+
